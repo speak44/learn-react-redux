b23f222bc27c51eb5e9616d59e9ddf08f73b055e
chensssss <cherrychen1@shunshunliuxue.com>
2020-07-13 15:36:56 +0800

    start

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from 'react'
+import { BrowserRouter as Router,Route, Link, Switch} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import HomePage from './pages/HomePage'
+import LoginPage from './pages/LoginPage'
+import UserPage from './pages/UserPage'
+import _404Page from './pages/_404Page'
+
+export default function App(){
+  return(
+    <div className="app">
+      <Router>
+        <Link to='/'>
+          首页
+        </Link>
+        <Link to='/user'>
+          用户中心
+        </Link> 
+        <Link to='/login'>
+          登陆
+        </Link>
+        <Link to="/product/123">
+          商品
+        </Link>
+
+        <Switch>
+         {/* 没有swtich  就会把匹配到的进行现实*/}
+           <Route exact path="/" 
+           children={()=><div>HomePage-children</div>}
+           component={HomePage}
+           render={()=><div>HomePage-render</div>}
+           ></Route>
+           {/* 优先级顺序 */}
+           {/* children>componentrender */}
+           <Route path="/user" component={UserPage}></Route>
+           <Route path="/login" component={LoginPage}></Route>
+           <Route component={_404Page}></Route>
+        </Switch>           
+      </Router>
     </div>
-  );
+  )
 }
-
-export default App;
